1d9b8067e26ba47ca1cd1016a8f347a207f5b6bf
Denis <gaz91.91@mail.ru>
2022-04-15 08:40:20 +0500

    Инициализировал пустой Avalonia UI проект

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Bipoly.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
